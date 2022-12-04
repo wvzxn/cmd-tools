7896082783af1c89a12b26f13e8e11f8c9000126
wvzxn <zvp.ukr01@gmail.com>
2022-12-04 20:45:28 +0200

    update

--- batchRename.cmd
+++ batchRename.cmd
@@ -1,24 +1,22 @@
-::         Name: batchRename v1.0
+::         Name: batchRename v1.1
 ::       Author: wvzxn // https://github.com/wvzxn/
-::  Description: The script will rename by query in current folder and subfolders.
+::  Description: Rename item by query in folder and subfolders.
+::               Drag'n'Drop is supported.
 ::               
-::               Find         = Files/Folders to find (wildcard, regex is not supported)
-::               Replace this = The characters to find (regex)
-::               To           = The characters to replace them with. (regex)
+::                       Find: Files/Folders to find (wildcard, regex is not supported)
+::               Replace this: The characters to find (regex)
+::                         By: The characters to replace them with (regex)
 
 @echo off
 for /f "usebackq delims=" %%A in (` findstr /b /c:"::  " "%~f0" `) do echo %%A
 echo.
 set /p "a=Find: "
 set /p "b=Replace this: "
-set /p "c=To: "
-if not exist "%~1" (
-    powershell "gci '%~f0' -filter '%a%' -recurse|rni -NewName {$_.name -replace '%b%','%c%'}"
-    pause
-    exit
-)
+set /p "c=By: "
+
 :loop
-powershell "gci '%~f1' -filter '%a%' -recurse|rni -NewName {$_.name -replace '%b%','%c%'}"
+if exist "%~1" ( set "d=%~f1") else ( set "d=%~dp0")
+powershell "gci '%d%' -filter '%a%' -recurse|rni -NewName {$_.name -replace '%b%','%c%'}"
 shift
 if exist "%~1" ( goto:loop)
 pause
